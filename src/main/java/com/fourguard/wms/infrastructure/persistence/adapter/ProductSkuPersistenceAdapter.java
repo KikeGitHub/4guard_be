9fc0489ef54f12c6ee95a9a77ed13eba921ffca6
Edgarsyborx <edgar.gonzalez@syborx.com>
2026-09-09 13:36:20 -0600

    Cambios en el modulo de cambio de almacen

--- src/main/java/com/fourguard/wms/infrastructure/persistence/adapter/ProductSkuPersistenceAdapter.java
+++ src/main/java/com/fourguard/wms/infrastructure/persistence/adapter/ProductSkuPersistenceAdapter.java
@@ -18,15 +18,15 @@ public class ProductSkuPersistenceAdapter implements ProductSkuRepositoryPort {
     private final ProductSkuJpaRepository repository;
 
     @Override
-    @Cacheable(value = "catalogues", key = "'sku-' + #id", unless = "#result == null || !#result.isPresent()")
+    @Cacheable(value = "catalogues", key = "'sku-' + #id", unless = "#result == null")
     public Optional<ProductSkuEntity> findById(UUID id)                              { return repository.findById(id); }
 
     @Override
-    @Cacheable(value = "catalogues", key = "'sku-client-code-' + #cid + '-' + #code", unless = "#result == null || !#result.isPresent()")
+    @Cacheable(value = "catalogues", key = "'sku-client-code-' + #cid + '-' + #code", unless = "#result == null")
     public Optional<ProductSkuEntity> findByClientIdAndCode(UUID cid, String code)   { return repository.findByClientIdAndCode(cid, code); }
 
     @Override
-    @Cacheable(value = "catalogues", key = "'sku-code-' + #code", unless = "#result == null || !#result.isPresent()")
+    @Cacheable(value = "catalogues", key = "'sku-code-' + #code", unless = "#result == null")
     public Optional<ProductSkuEntity> findFirstByCode(String code)                   { return repository.findFirstByCode(code); }
 
     @Override
